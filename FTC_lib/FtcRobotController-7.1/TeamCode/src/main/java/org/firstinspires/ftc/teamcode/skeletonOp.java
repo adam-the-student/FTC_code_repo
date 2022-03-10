f13660a6b99cdf76eeec89439986afefc5daf6b3
Adam Jaffery <adamjaffery4@gmail.com>
2022-03-10 12:57:38 -0600

    Make SkelotonOp which contains basic Op structure

---
+++ FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skeletonOp.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp(name = "Skeloton Op Mode", group = "TeleOp")
+public class skeletonOp extends OpMode{
+    @Override
+    public void init() {
+
+    }
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
