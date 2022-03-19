5e43a1aa38924a219f196f86617f7cdf5e1b966d
Fawwaz <fawwaz.shaphy@gmail.com>
2022-03-19 14:33:47 -0500

    put my code for square in there. - Fawwaz

--- FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/squareTest.java
+++ FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/squareTest.java
@@ -31,6 +31,7 @@ public class squareTest extends OpMode {
         motorSpeed(-5, 5, 5, -5); // this is left
     }
 
+
     @Override
     public void loop() {
 
@@ -42,8 +43,15 @@ public class squareTest extends OpMode {
     }
     public void motorSpeed(int speed1 , int speed2 , int speed3 , int speed4){
 
+        motor1.setVelocity(speed1);
+        motor2.setVelocity(speed2);
+        motor3.setVelocity(speed3);
+        motor4.setVelocity(speed4);
 
-
+        motor1.setTargetPosition(10);
+        motor2.setTargetPosition(10);
+        motor3.setTargetPosition(10);
+        motor4.setTargetPosition(10);
     }
 
 }
