29333e448de4d3a7580ca76441874f4162f337e7
Adam Jaffery <adamjaffery4@gmail.com>
2022-04-09 13:06:30 -0500

    Add Autotest.java to test encoders

---
+++ FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autotest.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Autonomous(name = "Autotest", group = "Autonomous")
+public class Autotest extends OpMode {
+//hardware maping
+    private DcMotorEx motor1;
+    private DcMotorEx motor2;
+    private DcMotorEx motor3;
+    private DcMotorEx motor4;
+
+    @Override
+    public void init() {
+
+
+        motor1 = hardwareMap.get(DcMotorEx.class, "motor1");
+        motor2 = hardwareMap.get(DcMotorEx.class, "motor2");
+        motor3 = hardwareMap.get(DcMotorEx.class, "motor3");
+        motor4 = hardwareMap.get(DcMotorEx.class, "motor4");
+    }
+
+
+    @Override
+    public void start() {
+        motor1.setVelocity(1);
+        motor2.setVelocity(1);
+        motor3.setVelocity(1);
+        motor4.setVelocity(1);
+        //we alternate motor direction to
+        motor1.setTargetPosition(100); //we control how far the motors turn.
+        motor2.setTargetPosition(100);
+        motor3.setTargetPosition(100);
+        motor4.setTargetPosition(100);
+
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+
+}
+
