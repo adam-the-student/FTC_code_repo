3bdd2790f1276639fe9d6752e14509ae591eeb4e
Fawwaz <fawwaz.shaphy@gmail.com>
2022-04-10 13:24:32 -0500

    un invert MOVE NEW. - Fawwaz

--- FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MOVE.java
+++ FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MOVE.java
@@ -30,23 +30,23 @@ public class MOVE extends LinearOpMode {
                 motor2.setPower(-gamepad1.right_stick_y);
                 motor3.setPower(gamepad1.right_stick_y);
 
-                double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-                double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+                double y = gamepad1.left_stick_y;
+                double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
                 double rx = gamepad1.right_stick_x;
 
                 // Denominator is the largest motor power (absolute value) or 1
                 // This ensures all the powers maintain the same ratio, but only when
                 // at least one is out of the range [-1, 1]
                 double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-                double motor1Power = (y + x + rx) / denominator; //motor1 is top left corner
-                double motor2Power = (y - x - rx) / denominator; //motor2 is top right corner
-                double motor3Power = (y - x + rx) / denominator; //motor3 is bottom left corner
-                double motor4Power = (y + x - rx) / denominator; //motor4 is bottom right corner
+                double motor1Power = (y + x + rx) / denominator;  //motor1 is top left corner
+                double motor2Power = (y - x - rx) / denominator;  //motor2 is top right corner
+                double motor3Power = (y - x + rx) / denominator;  //motor3 is bottom left corner
+                double motor4Power = (y + x - rx) / denominator;  //motor4 is bottom right corner
 
-                motor1.setPower(motor1Power); // motor1 is top left
-                motor2.setPower(-motor2Power); // motor2 is top right
-                motor3.setPower(motor3Power); // motor3 is bottom left
-                motor4.setPower(-motor4Power); // motor4 is bottom right
+                motor1.setPower(motor1Power);  // motor1 is top left
+                motor2.setPower(-motor2Power);  // motor2 is top right
+                motor3.setPower(motor3Power);  // motor3 is bottom left
+                motor4.setPower(-motor4Power);  // motor4 is bottom right
                 telemetry.update();
             }
         }
