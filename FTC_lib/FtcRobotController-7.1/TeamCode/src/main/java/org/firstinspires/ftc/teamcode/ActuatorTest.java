51e6bb642f37aec106e2acdbd3ba8493da73a2a1
Fawwaz <fawwaz.shaphy@gmail.com>
2022-05-14 14:17:00 -0500

    Reset ActuatorTest.java and RamadanRover.java - Fawwaz

---
+++ FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActuatorTest.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name = "Actuator Test", group = "Autonomous")
+
+public  class ActuatorTest extends LinearOpMode {
+
+}
