8343152c7d18806da93b6d7e696abae924017cd2
Fawwaz <fawwaz.shaphy@gmail.com>
2022-06-11 12:18:06 -0500

    Add stuff - Fawwaz

--- FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActuatorTest.java
+++ FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActuatorTest.java
@@ -1,11 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "Actuator Test", group = "Autonomous")
+@TeleOp(name = "extend ")
+public class extend1 extends LinearOpMode {
 
-public  class ActuatorTest extends LinearOpMode {
+    private Servo linearServo0;
 
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+    @Override
+    public void runOpMode() {
+        linearServo0 = hardwareMap.get(Servo.class, "linearServo0");
+
+        // Set servo to mid position
+        waitForStart();
+        while (opModeIsActive()) {
+            if (linearServo0.getPosition !=  0 && linearServo0 != 1) {
+                linearServo0.setPosition(gamepad1.left_stick_x);
+            }
+        }
+    }
 }
