6dd7816acaf5b19bdd58418dd0a67145c1d99ed4
Fawwaz <fawwaz.shaphy@gmail.com>
2022-07-23 14:25:32 -0500

    Clean Up Code - Fawwaz

--- FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NorthSummerCupFreightFrenzy.java
+++ FTC_lib/FtcRobotController-7.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NorthSummerCupFreightFrenzy.java
@@ -21,10 +21,8 @@ public class NorthSummerCupFreightFrenzy extends LinearOpMode {
         motor4 = hardwareMap.dcMotor.get("motor4");
         motor2 = hardwareMap.dcMotor.get("motor2");
         motor3 = hardwareMap.dcMotor.get("motor3");
-        // Put initialization blocks here.
-        int speedToggle = 0;
-        waitForStart();
 
+        waitForStart();
         if (opModeIsActive()) {
             // Put run blocks here.
 
@@ -33,15 +31,11 @@ public class NorthSummerCupFreightFrenzy extends LinearOpMode {
                 double y = gamepad1.left_stick_y;
                 double x = -gamepad1.left_stick_x * 1.15; // Counteract imperfect strafing
                 double rx = -gamepad1.right_stick_x;
-              /*Denominator is the largest motor power (absolute value) or 1
-                 This ensures all the powers maintain the same ratio, but only when
-                 at least one is out of the range [-1, 1]                   */
                 double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
                 double motor1Power = (y + x +  rx) / denominator;  //motor1 is top left corner
                 double motor2Power = (y - x - rx) / denominator;  //motor2 is top right corner
                 double motor3Power = (y - x + rx) / denominator;  //motor3 is bottom left corner
                 double motor4Power = (y + x -  rx) / denominator;  //motor4 is bottom right corner
-
                     motor1.setPower(motor1Power);  // motor1 is top left
                     motor2.setPower(-motor2Power);  // motor2 is top right
                     motor3.setPower(motor3Power);  // motor3 is bottom left
