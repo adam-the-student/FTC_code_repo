36ea4a8cb78f5a830de646904a099b2c23777600
Adam Jaffery <adamjaffery4@gmail.com>
2022-08-20 20:40:20 -0700

    change and disable CVencoders.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVencoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVencoders.java
@@ -6,19 +6,36 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @Disabled
-@TeleOp(name = "newencoder program", group = "TeleOp")
+@TeleOp(name = "newencoder program", group = "autonomus")
 public class CVencoders extends OpMode {
+
+    private DcMotorEx motor1;
+    private DcMotorEx motor2;
+    private DcMotorEx motor3;
+    private DcMotorEx motor4;
 
     @Override
     public void init() {
-
+        motor1 = hardwareMap.get(DcMotorEx.class, "motor1");
+        motor2 = hardwareMap.get(DcMotorEx.class, "motor2");
+        motor3 = hardwareMap.get(DcMotorEx.class, "motor3");
+        motor4 = hardwareMap.get(DcMotorEx.class, "motor4");
     }
+
+    public void get_encoder_x_change_1(){
+       double x = motor1.getCurrentPosition();
+       double y =motor1.getTargetPosition();
+    }
+
     @Override
     public void start() {
 
     }
+
+
 
     @Override
     public void loop() {
