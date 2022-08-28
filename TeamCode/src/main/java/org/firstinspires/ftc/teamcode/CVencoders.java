5b01c1744c99a0b3507d56962a501790821cadaf
Adam <Adamjaffery4@gmail.com>
2022-08-28 12:54:58 -0700

    Add opencv capabilllitys

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVencoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVencoders.java
@@ -1,49 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-@Disabled
-@TeleOp(name = "newencoder program", group = "autonomus")
-public class CVencoders extends OpMode {
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
 
-    private DcMotorEx motor1;
-    private DcMotorEx motor2;
-    private DcMotorEx motor3;
-    private DcMotorEx motor4;
-
+class EmptyPipeline extends OpenCvPipeline
+{
     @Override
-    public void init() {
-        motor1 = hardwareMap.get(DcMotorEx.class, "motor1");
-        motor2 = hardwareMap.get(DcMotorEx.class, "motor2");
-        motor3 = hardwareMap.get(DcMotorEx.class, "motor3");
-        motor4 = hardwareMap.get(DcMotorEx.class, "motor4");
-    }
-
-    public void get_encoder_x_change_1(){
-       double x = motor1.getCurrentPosition();
-       double y =motor1.getTargetPosition();
-    }
-
-    @Override
-    public void start() {
-
-    }
-
-
-
-    @Override
-    public void loop() {
-
-    }
-
-    @Override
-    public void stop() {
-
+    public Mat processFrame(Mat input)
+    {
+        return input;
     }
 }
